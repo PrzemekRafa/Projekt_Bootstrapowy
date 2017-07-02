3eb859568e050d6f90c8c69ab6d6096717e87a40
Przemek Rafa Mac <przemyslaw.rafa@gmail.com>
2017-07-02 20:32:39 +0200

    stage2-koniec-dnia-2

--- js/skrypty.js
+++ js/skrypty.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
 
     bckgrndShow();
+    scrollAnimate ();
     
 });
 
@@ -11,21 +12,37 @@ $(window).scroll (function () {
     bckgrndShow();
     
 });
-        
-var navHeight = $('.navbar').height();
-var currentPosition = $(window).scrollTop();
+$(window).resize(function (){
+    scrollAnimate ();
+});
 
 function bckgrndShow (){
+    var navHeight = $('.navbar').height();
     if ( $(window).scrollTop() > navHeight) {
         $('.navbar').addClass('navbar_temp');
     } else {
         $('.navbar').removeClass('navbar_temp');
-    }
+    } 
 };
 
+function scrollAnimate () {
+//var normalNavHeight = $('.navbar').height();
+//var burgerNavHeight = $('#bs-example-navbar-collapse-1').height();
 $(document).on('click', 'a', function(event){
     event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top - navHeight
-    }, 500);
+    if($(window).width() <= 768) {
+        var burgerNavHeight = $('#bs-example-navbar-collapse-1').height();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - burgerNavHeight
+        }, 500);
+    } else {
+        var normalNavHeight = $('.navbar').height();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - normalNavHeight
+        }, 500);
+    }
+//    console.log($(window).width());
+//    console.log(navHeight);
+//    console.log(burgerNavHeight);
 });
+}
